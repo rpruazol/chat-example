cb0d60d8ffe1d272cd44a620d7efb83fbdcb73d9
rpruazol <rpruazol@gmail.com>
2023-08-10 19:28:58 -0400

    basic chat window

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
+
 require('dotenv').config();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  })
 });
 
 server.listen(process.env.PORT, () => {
